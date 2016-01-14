a516fa589e012a8c61138f82edd92211278f9d84
RobertSmythKF <robert.smyth@knightfrank.com>
2016-01-14 10:51:05 +0000

    Initial Commit

---
+++ src/KnightFrank.Icon.MVC6.Web/Controllers/AppController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KnightFrank.Icon.MVC6.Web.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/KnightFrank.Icon.MVC6.Web/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KnightFrank.Icon.MVC6.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseMvc(config => {
+
+                config.MapRoute(
+
+                        name: "Default",
+                        template: "{controller}/{action}/{id?}",
+                        defaults: new { controller = "App", action = "Index" }
+                    );
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
