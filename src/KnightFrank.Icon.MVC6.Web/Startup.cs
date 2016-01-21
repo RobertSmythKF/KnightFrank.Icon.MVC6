2aa3775442f44dc51dbe478dc7339fbd410f9532
Andrew Wakeling <andrew.wakeling@knightfrank.com>
2016-01-21 12:12:14 +0000

    Revert

--- src/KnightFrank.Icon.MVC6.Web/Startup.cs
+++ src/KnightFrank.Icon.MVC6.Web/Startup.cs
@@ -10,8 +10,6 @@ namespace KnightFrank.Icon.MVC6.Web
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-			// I think we should be using classic ASP instead. What do you guys think?
-
             services.AddMvc();
         }
 
